94e19b1067e986ea79f9d23792785297fdb74205
AnTosi <andrea.tosi.uni@gmail.com>
2021-11-10 16:07:38 +0100

    fatto esercizio base

---
+++ first_project/src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="title">
+    <h1>
+        La mia prima app con Vue CLI!
+    </h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'firstTitle',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.title {
+    color: cornflowerblue;
+    background-color: blanchedalmond;
+}
+</style>
